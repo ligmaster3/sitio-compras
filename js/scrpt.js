03208de41b2d2cbe1e7d6df6db7f65d477a50af0
ligmaster <136215596+ligmaster3@users.noreply.github.com>
2024-04-04 21:41:31 -0500

    Update scrpt.js

---
+++ js/scrpt.js
@@ -0,0 +1,25 @@
+// fetch('js/productos.json')
+// .then(response => response.json())
+// .then(data => cargarProductos(data))
+// .catch(error => console.log(error));
+
+
+const tdsproductoslist = document.getElementById('productlist');
+const cargarProductos = document.getElementById('cartitem');
+const carttotal = document.getElementById('carttotal');
+
+let cart = JSON.parse(localStorage.getItem('cart')) ||[];
+
+function cargarProductos() {
+    productoElemento.innerHTML = productlist.map(
+        (product) =>  `<div class="product">
+        <img src="" alt="" class="product-img">
+        <div class="product-info">
+        <h2 class="product-title"></h2>
+        <p class="product-price"> </p>
+        <a class="add-to-cart">add to cart</a>
+        </div>
+    </div>
+    `)
+    .join("")
+}
